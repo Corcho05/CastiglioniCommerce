db4ec1e05c3e8a340c01cd658cbf99371a3f2e77
Sebastián Castiglioni <sebastiancastiglioni@gmail.com>
2022-09-06 12:04:15 -0300

    Uso de Context para el carrito

---
+++ src/context/cartContext.js
@@ -0,0 +1,55 @@
+import React, { useState, createContext } from 'react';
+
+const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+    const [cart, setCart] = useState([]);
+
+    const addItem = (item, quantity) => {
+        if (isInCart(item.id)) {
+            //Si existe hago un map para sumarle la cantidad que quiero agregar
+            const newCart = cart.map((cartItem) => {
+                if (cartItem.item.id === item.id) {
+                    return { item, quantity: cartItem.quantity + quantity };
+                } else {
+                    return cartItem;
+                }
+            });
+            setCart(newCart);
+        } else {
+            //Si no existe lo agrego al carrito
+            setCart([...cart, { item, quantity }]);
+        }
+    };
+
+    const removeItem = (itemId) => {
+        const newCart = cart.filter((cartItem) => cartItem.item.id !== itemId);
+        setCart(newCart);
+    };
+
+    const clear = () => {
+        setCart([]);
+    };
+
+    const isInCart = (itemId) => {
+        return cart.some((cartItem) => cartItem.item.id === itemId); //Devuelve true o false si existe el item en el carrito
+    };
+
+    return (
+        <CartContext.Provider
+            value={{
+                cart,
+                addItem,
+                removeItem,
+                clear,
+                isInCart
+            }}>
+            {children}
+        </CartContext.Provider>
+    );
+}
+
+export {
+    CartProvider
+}
+export default CartContext;
